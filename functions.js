1259d215d5b66e4ffa600005a8bc5a0d55481665
stephanie-niva <stephanienivaa@example.com>
2024-03-29 19:49:56 +0300

    add file

---
+++ functions.js
@@ -0,0 +1,14 @@
+//Question 1
+function sorting(){
+    const name = ['Stef','John','Eli'];
+    console.log(name.sort());
+    console.log(name.reverse())
+}
+//Question4
+function multiplication(numbers){
+    numbers.forEach((numbers)=>{
+        console.log(numbers*2);
+    });
+};
+multiplication((20,30,50,60));
+
